b4e1a526d58448a4bd956a0ff113840f044f7927
vt-pikaos <VintageSwordsman@hotmail.com>
2026-03-18 21:46:42 +1100

    v0.01
    General config changes, plus Make Leafgreen working.

--- src/new_game.c
+++ src/new_game.c
@@ -135,7 +135,7 @@ static void ClearFrontierRecord(void)
 static void WarpToTruck(void)
 {
     if (IS_FRLG)
-        SetWarpDestination(MAP_GROUP(MAP_PALLET_TOWN_PLAYERS_HOUSE_2F), MAP_NUM(MAP_PALLET_TOWN_PLAYERS_HOUSE_2F), WARP_ID_NONE, 6, 6);
+        SetWarpDestination(MAP_GROUP(MAP_STIKX), MAP_NUM(MAP_STIKX), WARP_ID_NONE, 15, 10);
     else
         SetWarpDestination(MAP_GROUP(MAP_INSIDE_OF_TRUCK), MAP_NUM(MAP_INSIDE_OF_TRUCK), WARP_ID_NONE, -1, -1);
     WarpIntoMap();
